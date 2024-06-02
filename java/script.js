3850f047dc272c80b9b3301a6b18c4c2c6d08edb
rosa16001543000 <111840039+shawarm5@users.noreply.github.com>
2024-06-02 12:02:41 +0300

    Add files via upload

---
+++ java/script.js
@@ -0,0 +1,4 @@
+document.getElementById("cv-link").addEventListener("click", function(event) {
+  event.preventDefault(); // Prevent the default behavior of the link
+  window.location.href = "https://www.youtube.com/watch?v=xvFZjo5PgG0"; // Redirect to the Rickroll link
+});
